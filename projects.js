60167f07eefe0f9d63bc192f1451b02e160ee538
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-07-27 17:51:41 +0300

    Set up the projects.js file with the project-making factory function as well as a find project by ID, and add/remove task from project

---
+++ projects.js
@@ -0,0 +1,62 @@
+let projects = [];
+
+function createProjectFactory(title, description, dueDate, completedStatus = false, tasksArray = []) {
+    let _title = title;
+    let _description = description;
+    let _dueDate = dueDate;
+    let _completedStatus = completedStatus;
+    let _tasksArray = tasksArray;
+    let _id = crypto.randomUUID();
+
+    return {
+        getTitle() { 
+            return _title;
+        },
+        setTitle(newTitle) { 
+            _title = newTitle; 
+        },
+        getDescription() {
+            return _description;
+        },
+        setDescription(newDescription) {
+            _description = newDescription;
+        },
+        getDueDate() {
+            return _dueDate;
+        },
+        setDueDate(newDueDate) {
+            _dueDate = newDueDate;
+        },
+        getCompletedStatus() {
+            return _completedStatus
+        },
+        setCompletedStatus() {
+            _completedStatus = !_completedStatus;
+        },
+        getTasks() {
+            return _tasksArray;
+        },
+        pushTask(task) {
+            return _tasksArray.push(task);
+        },
+        getID() {
+            return _id;
+        }
+    }
+}
+
+function findProjectByID(projectID) {
+    for (const project of projects) {
+        if (project.getID() === projectID) {
+            return project;
+        }
+    }
+    return 'Incorrect projectID or project not found'
+}
+
+function addTaskToProject(projectID, taskID) {
+}
+
+function removeTaskFromProject(projectID, taskID) {
+
+}
