839857a2ea18b1e493a4b7e6245e80aa8f4934d2
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-07-27 16:17:43 +0300

    Set up the to-do task object factory function

---
+++ script.js
@@ -0,0 +1,47 @@
+function createTodo(title, description, dueDate, priority, project, completedStatus = false) {
+    let _title = title;
+    let _description = description;
+    let _dueDate = dueDate;
+    let _priority = priority;
+    let _project = project;
+    let _completedStatus = completedStatus;
+    
+    return {
+        getTitle() { 
+            return _title;
+        },
+        setTitle(newTitle) { 
+            _title = newTitle; 
+        },
+        getDescription() {
+            return _description;
+        },
+        setDescription(newDescription) {
+            _description = newDescription;
+        },
+        getDueDate() {
+            return _dueDate;
+        },
+        setDueDate(newDueDate) {
+            _dueDate = newDueDate;
+        },
+        getPriority() {
+            return _priority;
+        },
+        setPriority(newPriority) {
+            _priority = newPriority;
+        },
+        getProject() {
+            return _project;
+        },
+        setProject(newProject) {
+            _project = newProject;
+        },
+        getCompletedStatus() {
+            return _completedStatus
+        },
+        setCompletedStatus(newCompletedStatus) {
+            _completedStatus = newCompletedStatus;
+        }
+    }
+}
