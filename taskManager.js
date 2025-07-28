699d7e276a946338c6b766330dc9e0a660cf496b
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-07-28 17:28:52 +0300

    Add base caching and binding events for the project and task dialogs

--- taskManager.js
+++ taskManager.js
@@ -13,10 +13,22 @@ const taskManager = {
     },
 
     cacheDom: function() {
+        this.addProjectBtn = document.querySelector("#add-project-btn");
+        this.projectDialog = document.querySelector('#add-project-dialog');
+        this.projectForm = document.querySelector('.add-project-form');
+        this.addTaskBtn = document.querySelector("#add-task-btn");
+        this.taskDialog = document.querySelector('#add-task-dialog');
+        this.taskForm = document.querySelector('.add-task-form');
+        this.projectCancelBtn = document.querySelectorAll('.project-cancel-btn');
+        this.taskCancelBtn = document.querySelectorAll('.task-cancel-btn');
+
 
     },
     bindEvents: function() {
-
+        this.addProjectBtn.addEventListener("click", () => this.projectDialog.showModal());
+        this.projectCancelBtn.addEventListener("click", () => this.projectDialog.close());
+        this.addTaskBtn.addEventListener("click", () => this.taskDialog.showModal());
+        this.taskCancelBtn.addEventListener("click", () => this.taskDialog.close());
     },
     render: function() {
 
